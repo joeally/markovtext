d78781df980eb72059215b2eae88b6a14b864020
(Alexander) Joe Ally <ajally1@gmail.com>
2014-09-18 20:45:43 +0100

    renamed transitiont.clj to TransitionTable.clj

---
+++ src/markovtext/TransitionTable.clj
@@ -0,0 +1,29 @@
+(ns markovtext.TransitionTable
+  (:gen-class))
+
+(defn concat-bag [bag1 bag2]
+  (merge-with + bag1 bag2))
+
+(defprotocol TransitionTable
+  "A protocol for a transition table"
+  (rand-next [tt prev])
+  (rand-first [tt])
+  (concat-tt [tt1 tt2])
+  (add-transition [tt from to]))
+
+(extend-protocol TransitionTable
+  clojure.lang.IPersistentMap
+
+  (add-transition [tt from to]
+    (update-in tt [from to] (fnil inc 0)))
+
+  (concat-tt [tt1 tt2]
+    (merge-with concat-bag tt1 tt2))
+
+  (rand-first [tt]
+    (rand-nth (keys tt)))
+
+  (rand-next [tt prev]
+    (try
+      (rand-nth (apply concat (for [[k n] (get tt prev)] (repeat n k))))
+      (catch IndexOutOfBoundsException e nil))))
